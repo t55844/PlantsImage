42b2ded3fc0be104af582f6032502f4d1fa56a83
thiago <thiagosoares111@yahoo.com.br>
2023-06-17 20:11:19 -0300

    construindo a seção das plantas

--- components/Home/MainSection.tsx
+++ components/Home/MainSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import Button from '../GeneralComponents/Button';
 
 export default function MainSection(props) {
@@ -11,6 +11,7 @@ export default function MainSection(props) {
                     source={require('../../assets/home-flowers.webp')}
                 />
             </View>
+            <Text className="text-white p-2 text-xl">Many plant species have medicinal properties and are used in traditional medicine. For example, the bark of the willow tree contains salicin, a compound used to develop aspirin.</Text>
 
             <Button
                 title='"Learn More"'
--- components/Home/PageHome.tsx
+++ components/Home/PageHome.tsx
@@ -5,10 +5,9 @@ import FooterComponent from './FoterSection';
 
 export default function PageHome(props) {
     return (
-        <View className="flex flex-col justify-between items-center m-2 h-screen bg-gradient-to-b from-emerald-700 via-emerald-400 to-emerald-200">
+        <View className="flex flex-col justify-between items-center pt-6 h-screen bg-emerald-500">
             <View className="">
-                <Text className=" text-white text-center text-xl border-b-4 border-white mb-2 " >Welcome to PlantsImage !!!</Text>
-                <Text className=" text-white text-2xll  mb-5" >see several plants and their main information with a click</Text>
+                <Text className=" text-white text-center text-3xl border-b-4 border-white mb-2 " >Welcome to PlantsImage !!!</Text>
             </View>
             <MainSection></MainSection>
             <FooterComponent></FooterComponent>
